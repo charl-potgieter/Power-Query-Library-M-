2343819de52a591a729e2a7d0ed5ad683df21306
Charl Potgieter <charl.j.potgieter@gmail.com>
2019-10-31 21:15:39 +1100

    updates

--- GeneratingTablesFromCode.m
+++ GeneratingTablesFromCode.m
@@ -11,20 +11,19 @@ let
     tbl_FromRecords = Table.FromRecords({[Header1=1, Header2=2, Header3=3], [Header1=2, Header2=5, Header3=7]}),
 
 	
-	//Credit Chris Webb
-	//https://blog.crossjoin.co.uk/2016/06/03/creating-tables-in-power-bipower-query-m-code-using-table/
-	tbl_WithTypes = #table(
-		type table
-			[
-				#"Number Column"=number, 
-				#"Text Column"=text,
-				#"Date Column"=date
-			], 
-		 {
-		  {1,"Hello",#date(2016,1,1)},
-		  {2,"World",#date(2017,12,12)}  
-		 }
-		)
+    //Credit Chris Webb
+    //https://blog.crossjoin.co.uk/2016/06/03/creating-tables-in-power-bipower-query-m-code-using-table/
+    tbl_WithTypes = #table(
+        type table[
+            #"Number Column"=number, 
+            #"Text Column"=text,
+            #"Date Column"=date
+            ], 
+        {
+            {1,"Hello",#date(2016,1,1)},
+            {2,"World",#date(2017,12,12)}
+        }
+    )
 	
 	
 in
